178fb952c1bc07b333f01ccea25866cd52e0aafc
syldium <syldium@mailo.com>
2023-11-27 21:24:10 +0100

    Limit the number of bytes read before killing the process

--- backend/src/runner.rs
+++ backend/src/runner.rs
@@ -1,9 +1,13 @@
 use futures::Stream;
+use std::pin::Pin;
 use std::process::Stdio;
+use std::task::{Context, Poll};
 use std::time::Duration;
-use tokio::io::{AsyncBufReadExt, AsyncWriteExt, BufReader};
+use tokio::io::{AsyncBufReadExt, AsyncRead, AsyncWriteExt, BufReader, ReadBuf};
 use tokio::process::Command;
 use tokio::time::timeout;
+
+const SIZE_LIMIT: usize = 256 * 1024; // 256 Kio
 
 pub(crate) async fn runner(code: String) -> impl Stream<Item = Option<String>> {
     async_stream::stream! {
@@ -50,8 +54,8 @@ pub(crate) async fn runner(code: String) -> impl Stream<Item = Option<String>> {
         stdin.write_all(code.as_bytes()).await.unwrap();
         drop(stdin);
 
-        let mut stdout = BufReader::new(child.stdout.take().unwrap()).lines();
-        let mut stderr = BufReader::new(child.stderr.take().unwrap()).lines();
+        let mut stdout = BufReader::new(TakeBytes::new(child.stdout.take().unwrap(), SIZE_LIMIT)).lines();
+        let mut stderr = BufReader::new(TakeBytes::new(child.stderr.take().unwrap(), SIZE_LIMIT)).lines();
         // Redirect stdout and stderr to the SSE stream
         loop {
             tokio::select! {
@@ -83,5 +87,38 @@ pub(crate) async fn runner(code: String) -> impl Stream<Item = Option<String>> {
             }
         };
         yield None;
+    }
+}
+
+struct TakeBytes<S: AsyncRead> {
+    inner: S,
+    seen: usize,
+    limit: usize,
+}
+
+impl<S: AsyncRead> TakeBytes<S> {
+    fn new(inner: S, limit: usize) -> Self {
+        Self {
+            inner,
+            seen: 0,
+            limit,
+        }
+    }
+}
+
+impl<S: AsyncRead + Unpin> AsyncRead for TakeBytes<S> {
+    fn poll_read(
+        mut self: Pin<&mut Self>,
+        cx: &mut Context<'_>,
+        buf: &mut ReadBuf<'_>,
+    ) -> Poll<std::io::Result<()>> {
+        if self.seen >= self.limit {
+            return Poll::Ready(Ok(()));
+        }
+        let poll = Pin::new(&mut self.inner).poll_read(cx, buf);
+        if let Poll::Ready(Ok(())) = poll {
+            self.seen += buf.filled().len();
+        }
+        poll
     }
 }
